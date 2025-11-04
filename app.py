a60ab0921094000010c372a38af7ed857207d337
FranciscoJavierSevillaSanchez <l19590678@sjuanrio.tecnm.mx>
2025-11-04 17:24:41 -0600

    Se pueden modificar los alumnos

--- app.py
+++ app.py
@@ -63,5 +63,20 @@ def create_alumno():
     return render_template('create_alumno.html')
 
 
+#Actualizar alumno
+@app.route('/alumnos/update/<string:no_control>', methods=['GET','POST'])
+def update_alumno(no_control):
+    alumno = Alumno.query.get(no_control)
+    if request.method == 'POST':
+        alumno.nombre = request.form['nombre']
+        alumno.ap_paterno = request.form['ap_paterno']
+        alumno.ap_materno = request.form['ap_materno']
+        alumno.semestre = request.form['semestre']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('update_alumno.html', alumno=alumno)
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
